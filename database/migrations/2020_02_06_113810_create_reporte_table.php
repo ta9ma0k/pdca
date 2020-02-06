d8cead47e09c74bd8d694935a43a411a5e95b07b
takuma <0ismore@gmail.come>
2020-02-07 07:20:32 +0900

    WIP ユーザー認証機能作成中

---
+++ database/migrations/2020_02_06_113810_create_reporte_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReporteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->char('id', 26)->primary();
+            $table->date('date');
+            $table->string('goal');
+            $table->string('good_point')->nullable();
+            $table->string('bad_point')->nullable();
+            $table->string('next_action')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('folders');
+    }
+}
